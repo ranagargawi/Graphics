a4c117404c1c619ab1c4769a3ad9780bd217a5df
Ranagargawi <ranagargawi@gmail.com>
2025-05-26 10:38:28 +0300

    added shahar's readImage.

--- mini/readImage.cpp
+++ mini/readImage.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 using namespace cv;
+// new variables for the option to change the camera position
+float camX = 400, camY = 300, camZ = 800;   // Eye position
+float yaw = 0.0f;   // Horizontal angle (in degrees)
+float pitch = -30.0f; // Vertical angle
+float moveSpeed = 10.0f;
+float turnSpeed = 5.0f;
 
 struct MyVec3f {
         float x, y, z;
@@ -35,6 +41,7 @@ vector<MyVec3f> vertices;
 vector<MyVec3f> normals;
 vector<unsigned int> indices;
 Mat heightMap;
+std::vector<int> pickedIDs;; //NEW- picking list
 
 // Create the mesh
 void buildMesh(float heightScale = 2.0f) {
@@ -89,6 +96,12 @@ void buildMesh(float heightScale = 2.0f) {
         normals[indices[i]] = normal;
         normals[indices[i + 1]] = normal;
         normals[indices[i + 2]] = normal;
+
+        // chat offerd for imporvemant
+        // normals[indices[i]] = normals[indices[i]] + normal;
+        // normals[indices[i + 1]] = normals[indices[i + 1]] + normal;
+        // normals[indices[i + 2]] = normals[indices[i + 2]] + normal;
+
     }
 }
 
@@ -96,9 +109,23 @@ void buildMesh(float heightScale = 2.0f) {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(400, 300, 800,   // Eye position: directly above center
-        400, 300, 0,     // Look-at position: center of the terrain
-        0, 1, 0);        // Up vector: +Y is up on screen
+    // gluLookAt(400, 300, 800,   // Eye position: directly above center
+    //     400, 300, 0,     // Look-at position: center of the terrain
+    //     0, 1, 0);        // Up vector: +Y is up on screen
+    
+    // Compute direction vector from yaw and pitch
+    float radYaw = yaw * M_PI / 180.0f;
+    float radPitch = pitch * M_PI / 180.0f;
+    float dirX = cos(radPitch) * cos(radYaw);
+    float dirY = sin(radPitch);
+    float dirZ = cos(radPitch) * sin(radYaw);
+
+    // Target the camera is looking at
+    float targetX = camX + dirX;
+    float targetY = camY + dirY;
+    float targetZ = camZ + dirZ;
+
+    gluLookAt(camX, camY, camZ, targetX, targetY, targetZ, 0, 1, 0);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
@@ -106,12 +133,26 @@ void display() {
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
     glBegin(GL_TRIANGLES);
-    for (size_t i = 0; i < indices.size(); ++i) {
-        const MyVec3f &n = normals[indices[i]];
-        const MyVec3f &v = vertices[indices[i]];
-        glNormal3f(n.x, n.y, n.z);
-        glVertex3f(v.x, v.y, v.z);
+    for (size_t i = 0; i < indices.size(); i+=3) {// chat gpt changed it to be i+=3
+        int triID = i / 3;
+        if (std::find(pickedIDs.begin(), pickedIDs.end(), triID) != pickedIDs.end()) {
+            glColor3f(1.0f, 0.0f, 0.0f); // Red highlight
+        } else {
+            glColor3f(0.6f, 0.6f, 0.6f); // Default gray
+        }
+        // const MyVec3f &n = normals[indices[i]];
+        // const MyVec3f &v = vertices[indices[i]];
+        // glNormal3f(n.x, n.y, n.z);
+        // glVertex3f(v.x, v.y, v.z);
+        for (int j = 0; j < 3; ++j) { //chat added the for loop
+            const MyVec3f &n = normals[indices[i + j]];
+            const MyVec3f &v = vertices[indices[i + j]];
+            glNormal3f(n.x, n.y, n.z);
+            glVertex3f(v.x, v.y, v.z);
+        }
+
     }
+    
     glEnd();
 
     glutSwapBuffers();
@@ -125,6 +166,127 @@ void reshape(int w, int h) {
     glMatrixMode(GL_MODELVIEW);
 }
 
+//NEW - picking option
+void renderForPicking() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    //gluLookAt(400, 300, 800, 400, 300, 0, 0, 1, 0); //constant camera view version
+
+    float radYaw = yaw * M_PI / 180.0f;
+    float radPitch = pitch * M_PI / 180.0f;
+    float dirX = cos(radPitch) * cos(radYaw);
+    float dirY = sin(radPitch);
+    float dirZ = cos(radPitch) * sin(radYaw);
+
+    gluLookAt(camX, camY, camZ, 
+              camX + dirX, camY + dirY, camZ + dirZ, 
+              0, 1, 0);
+
+    glDisable(GL_LIGHTING);
+
+    glBegin(GL_TRIANGLES);
+    for (size_t i = 0; i < indices.size(); i += 3) {
+        int id = i / 3;
+        int r = (id & 0x000000FF);
+        int g = (id & 0x0000FF00) >> 8;
+        int b = (id & 0x00FF0000) >> 16;
+        glColor3ub(r, g, b);
+
+        for (int j = 0; j < 3; ++j) {
+            const MyVec3f &v = vertices[indices[i + j]];
+            glVertex3f(v.x, v.y, v.z);
+        }
+    }
+    glEnd();
+    glFlush(); // Makes sure all commands are finished
+}
+
+
+// mouse click option for picking
+void mouseClick(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        GLint viewport[4];
+        glGetIntegerv(GL_VIEWPORT, viewport);
+
+        renderForPicking();  // Draw the scene using ID colors
+
+        // Read the pixel under the mouse
+        GLubyte pixel[4];
+        glReadPixels(x, viewport[3] - y, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, pixel);
+
+        int id = pixel[0] + pixel[1] * 256 + pixel[2] * 256 * 256;
+
+        // Add the picked id only if valid and not already picked
+        if (id >= 0 && std::find(pickedIDs.begin(), pickedIDs.end(), id) == pickedIDs.end()) {
+            pickedIDs.push_back(id);
+        
+        }
+        glutPostRedisplay(); // Ask OpenGL to redraw the scene
+    }
+}
+
+//keyboard function to move around - copyed from chatgpt
+void keyboard(unsigned char key, int x, int y) {
+    float radYaw = yaw * M_PI / 180.0f;
+    float radPitch = pitch * M_PI / 180.0f;
+
+    // Forward direction (ignores pitch for flat movement)
+    float dirX = cos(radYaw);
+    float dirZ = sin(radYaw);
+    float rightX = -dirZ;
+    float rightZ = dirX;
+
+    switch (key) {
+    case 27: // ESC
+        exit(0);
+    case 'w':
+        camX += dirX * moveSpeed;
+        camZ += dirZ * moveSpeed;
+        break;
+    case 's':
+        camX -= dirX * moveSpeed;
+        camZ -= dirZ * moveSpeed;
+        break;
+    case 'a':
+        camX += rightX * moveSpeed;
+        camZ += rightZ * moveSpeed;
+        break;
+    case 'd':
+        camX -= rightX * moveSpeed;
+        camZ -= rightZ * moveSpeed;
+        break;
+    case 'q': // Up
+        camY += moveSpeed;
+        break;
+    case 'e': // Down
+        camY -= moveSpeed;
+        break;
+    }
+    glutPostRedisplay();
+}
+
+void specialKeys(int key, int x, int y) {
+    switch (key) {
+    case GLUT_KEY_LEFT:
+        yaw -= turnSpeed;
+        break;
+    case GLUT_KEY_RIGHT:
+        yaw += turnSpeed;
+        break;
+    case GLUT_KEY_UP:
+        pitch += turnSpeed;
+        if (pitch > 89.0f) pitch = 89.0f;
+        break;
+    case GLUT_KEY_DOWN:
+        pitch -= turnSpeed;
+        if (pitch < -89.0f) pitch = -89.0f;
+        break;
+    }
+    glutPostRedisplay();
+}//end of Chatgpt
+
+
 // Entry point
 int main(int argc, char** argv) {
     heightMap = imread(argv[1], IMREAD_GRAYSCALE);
@@ -134,7 +296,7 @@ int main(int argc, char** argv) {
     }
     cv::equalizeHist(heightMap, heightMap);
 
-       cv::GaussianBlur(heightMap, heightMap, cv::Size(3, 3), 0);
+       //cv::GaussianBlur(heightMap, heightMap, cv::Size(3, 3), 0);
    cv::GaussianBlur(heightMap, heightMap, cv::Size(3, 3), 0);
 
     imshow("Processed Heightmap", heightMap);
@@ -167,6 +329,9 @@ waitKey(1000);
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutMouseFunc(mouseClick);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(specialKeys);
     glutMainLoop();
 
     return 0;
